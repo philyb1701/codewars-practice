0744dfadd71f40e9f76e3d6bd50ba984398a5b92
Phil Burkhill <philip.burkhill@gmail.com>
2022-02-21 13:57:01 +0000

    some more kyu8

---
+++ kyu8/find-remainder.js
@@ -0,0 +1,4 @@
+function remainder(n, m){
+    return (n > m ? n % m : m % n);  
+  }
+  //? makes it a ternary operator (<condition> ? <value1> : <value2>;). condition is conditional expression. if condition is true, then value 1 is executed and then value 2 if value 1 is false.
